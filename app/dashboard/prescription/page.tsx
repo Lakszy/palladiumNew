31d572337fae9a61a1f6a60764165879a7fb3dd6
Lakshay Khattar <lakshaykhattar0208@gmail.com>
2024-01-09 16:03:20 +0530

    hover

--- app/dashboard/prescription/page.tsx
+++ app/dashboard/prescription/page.tsx
@@ -6,10 +6,8 @@ import patient from "@/app/assets/images/patient.svg";
 import Image from "next/image";
 import { PhoneCall, Mail, ArrowBigUp } from "lucide-react";
 import { Thermometer, Ruler, Weight, Activity, Brain, Stethoscope, Plus } from 'lucide-react';
-import { IoMdSearch } from "react-icons/io";
 import { FiSearch } from "react-icons/fi";
 function index() {
-
     const prescription = [
         'Asthma Inhaler',
         'Antibiotics',
@@ -29,7 +27,6 @@ function index() {
         { icon: <Stethoscope />, text: "34 Kg.m2", scale: "" },
         { icon: <Plus />, text: "Add Other", scale: "" },
     ];
-
     return (
         <>
             <div className="p-4 ">
@@ -45,7 +42,7 @@ function index() {
                     <div className="gap-x-3 flex">
                         <div className="flex h-full border items-center rounded-full bg-lightPrimary text-navy-700 dark:bg-navy-900 dark:text-white xl:w-[325px]">
                             <p className="pl-3 pr-2 text-xl">
-                                <FiSearch className="h-4 w-4 text-gray-400 dark:text-white" />
+                                <FiSearch className="h-4 w-4 text-gray-400 dark:text-white"/>
                             </p>
                             <input
                                 type="text"
@@ -53,7 +50,6 @@ function index() {
                                 className="block h-full w-full rounded-full bg-lightPrimary text-sm font-medium text-navy-700 outline-none placeholder:!text-gray-400 dark:bg-navy-900 dark:text-white dark:placeholder:!text-white sm:w-fit"
                             />
                         </div>
-
                         <div className="w-[130px] h-[55px] px-[21px] py-[3px] bg-blue-700 bg-opacity-10 rounded-[30px] border border-blue-700 justify-start items-center gap-2 inline-flex">
                             <div className="justify-start items-center gap-2 flex">
                                 <MdSaveAlt className="text-blue-700" />
@@ -69,9 +65,7 @@ function index() {
                         </div>
                     </div>
                 </div>
-
-
-                <div className="w-[75rem] ml-2 justify-between px-2 border-gray-200 rounded-3xl  bg-white border-2 h-[177px] items-center gap-3 inline-flex">
+                <div className="w-full ml-2 justify-between px-2 border-gray-200 rounded-3xl  bg-white border-2 h-[177px] items-center gap-3 inline-flex">
                     <div className="gap-x-4 flex">
                         <Image
                             alt="patient"
@@ -107,17 +101,17 @@ function index() {
                         </div>
                     </div>
                     <div className="flex items-center gap-x-4 -mt-[1rem] mr-6">
-                        <div className="p-[18px] bg-white rounded-[30px] border border-stone-200 justify-start items-start gap-2.5 flex w-fit">
+                        <div className="hover:cursor-pointer p-[18px] bg-white rounded-[30px] border border-stone-200 justify-start items-start gap-2.5 flex w-fit">
                             <PhoneCall className="text-gray-400" />
                         </div>
-                        <div className="px-4 py-[18px] bg-white rounded-[30px] border border-stone-200 justify-start items-start gap-2.5 flex w-fit">
+                        <div className="hover:cursor-pointer px-4 py-[18px] bg-white rounded-[30px] border border-stone-200 justify-start items-start gap-2.5 flex w-fit">
                             <Mail className="text-gray-400" />
                         </div>
                     </div>
                 </div>
-                <div className="w-[1051px] mt-5  whitespace-nowrap inline-flex gap-3 gap-x-4 ml-3">
+                <div className="w-full mt-5  whitespace-nowrap inline-flex gap-3 gap-x-4 ml-3">
                     {prescription.map((medication, index) => (
-                        <div key={index} className="bg-white border border-stone-200 rounded-[30px] px-6 py-2">
+                        <div key={index} className="bg-white border hover:cursor-pointer border-stone-200 rounded-[30px] px-6 py-2">
                             <div className="text-zinc-800 text-opacity-80 text-lg font-medium ">
                                 {medication}
                             </div>
@@ -134,15 +128,14 @@ function index() {
                         className="block text-lg h-full w-full rounded-full bg-lightPrimary font-medium text-navy-700 outline-none placeholder:!text-gray-600 dark:bg-navy-900 dark:text-white dark:placeholder:!text-white "
                     />
                 </div>
-
                 <div className="grid grid-cols-[65%_35%] h-fit mt-2 gap-x-4 pr-10 ">
                     <div className="border-2 rounded-lg ">
                         <h1 className="text-[22px] font-semibold text-gray-500 p-2 pl-5">Vitals</h1>
-                        <div className="w-[759px] h-[122px] justify-start items-start gap-x-3 gap-y-2 inline-flex flex-wrap ml-4 mb-4">
+                        <div className="w-full h-[122px] justify-start items-start gap-x-3 gap-y-2 inline-flex flex-wrap ml-4 mb-4">
                             {vitals.map((vital, index) => (
                                 <div
                                     key={index}
-                                    className="w-fit h-[55px] px-[10px] py-[3px] bg-white rounded-[30px] border border-stone-200 justify-start items-center gap-3 flex "
+                                    className="w-fit hover:cursor-pointer h-[55px] px-[10px] py-[3px] bg-white rounded-[30px] border border-stone-200 justify-start items-center gap-3 flex "
                                 >
                                     {vital.icon && (
                                         <div className="justify-start items-center gap-2 flex ">
@@ -158,22 +151,15 @@ function index() {
                                 </div>
                             ))}
                             <div
-
                                 className="  w-fit h-[55px] px-[10px] py-[3px]  rounded-[30px] border border-stone-200 justify-start items-center gap-3 flex "
                             >
-
-                                <div className="text-red-500  text-lg font-semibold whitespace-nowrap">
+                                <div className="text-red-500  text-lg font-semibold whitespace-nowrap hover:cursor-pointer">
                                     Remove
                                 </div>
-
                             </div>
-
-
-
-
                         </div>
-                        <div className="ml-3 pl-3 w-[739px] h-[244px] py-[22px] bg-green-500 bg-opacity-20 rounded-[22px] flex-col justify-start items-start gap-2.5 inline-flex mt-3">
-                            <div className="flex-col justify-start items-start gap-5 flex">
+                        <div className="w-full  py-[22px] bg-green-500 bg-opacity-20 rounded-[22px] flex-col justify-start items-start gap-2.5 inline-flex mt-3">
+                            <div className="flex-col justify-start items-start gap-5 flex px-10">
                                 <div className="text-zinc-800 text-xl font-bold  leading-7">Complaints</div>
                                 <div className="flex-col justify-start items-start gap-[26px] flex">
                                     <div className="w-[659px] h-[13px] relative">
@@ -201,22 +187,22 @@ function index() {
                                         <div className="left-[618px] top-0 absolute text-zinc-800 text-md font-semibold  whitespace-nowrap">Sep 12, 2022</div>
                                     </div>
                                 </div>
-                                <div className="flex gap-x-2">
+                                <div className="flex gap-x-2 mt-5 mb-5">
                                     <div className="h-[55px] px-[21px] py-[3px] bg-white rounded-[30px] border border-stone-200 justify-start items-center gap-3 inline-flex">
-                                        <div className="justify-start items-center gap-2 flex">
+                                        <div className="justify-start items-center gap-2 flex hover:cursor-pointer">
                                             <Plus />
                                             <div className="text-zinc-800 text-opacity-80 text-lg font-semibold ">Add new </div>
                                         </div>
 
                                     </div>
-                                    <div className="h-[55px] px-[21px] py-[3px] text-red-500 bg-white rounded-[30px] border border-stone-200 justify-start items-center gap-3 inline-flex">
+                                    <div className=" hover:cursor-pointer h-[55px] px-[21px] py-[3px] text-red-500 bg-white rounded-[30px] border border-stone-200 justify-start items-center gap-3 inline-flex">
                                         <div className="text-red-500 text-opacity-80 text-lg font-semibold ">Remove </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="ml-3 pl-3 w-[739px] h-[244px] py-[22px] bg-green-500 bg-opacity-20 rounded-[22px] flex-col justify-start items-start gap-2.5 inline-flex mt-3">
-                            <div className="flex-col justify-start items-start gap-5 flex">
+                        <div className="w-full -translate-x-2 translate-x-2 py-[22px] bg-green-500 bg-opacity-20 rounded-[22px] flex-col justify-start items-start gap-2.5 inline-flex mt-3">
+                            <div className="flex-col justify-start items-start gap-5 flex px-10">
                                 <div className="text-zinc-800 text-xl font-bold  leading-7">Complaints</div>
                                 <div className="flex-col justify-start items-start gap-[26px] flex">
                                     <div className="w-[659px] h-[13px] relative">
@@ -244,29 +230,42 @@ function index() {
                                         <div className="left-[618px] top-0 absolute text-zinc-800 text-md font-semibold  whitespace-nowrap">Sep 12, 2022</div>
                                     </div>
                                 </div>
-                                <div className="flex gap-x-2">
+                                <div className="flex gap-x-2 mt-5 mb-5">
                                     <div className="h-[55px] px-[21px] py-[3px] bg-white rounded-[30px] border border-stone-200 justify-start items-center gap-3 inline-flex">
-                                        <div className="justify-start items-center gap-2 flex">
+                                        <div className="justify-start items-center gap-2 flex hover:cursor-pointer">
                                             <Plus />
                                             <div className="text-zinc-800 text-opacity-80 text-lg font-semibold ">Add new </div>
                                         </div>
 
                                     </div>
-                                    <div className="h-[55px] px-[21px] py-[3px] text-red-500 bg-white rounded-[30px] border border-stone-200 justify-start items-center gap-3 inline-flex">
+                                    <div className=" hover:cursor-pointer h-[55px] px-[21px] py-[3px] text-red-500 bg-white rounded-[30px] border border-stone-200 justify-start items-center gap-3 inline-flex">
                                         <div className="text-red-500 text-opacity-80 text-lg font-semibold ">Remove </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                       
                     </div>
-
-
-
                     <div className="border-2 pl-5 pt-3 bg-white rounded-lg overflow-y-auto pr-10">
-                        <div className="w-[343px] h-[49px] px-[62px] py-5 bg-emerald-100 rounded-xl justify-center items-center gap-2.5 inline-flex">
+                        <div className="w-full h-[49px] px-[62px] py-5 bg-emerald-100 rounded-xl justify-center items-center gap-2.5 inline-flex">
                             <div className="text-zinc-800 text-md font-bold ">Suggestive Medications</div>
                         </div>
-                        <div className="border-2 w-fit flex-col justify-start items-start gap-2.5 inline-flex p-2 m-2 bg-gray-100 rounded-xl">
+                        <div className="border-2 w-full flex-col justify-start items-start gap-2.5 inline-flex p-2 m-2 bg-gray-100 rounded-xl">
+                            <div className="w-full justify-start items-center gap-1.5 inline-flex">
+                                <div className="w-[35px] h-[35px] bg-stone-100 rounded-full flex items-center justify-center">
+                                    <ArrowBigUp />
+                                </div>
+                                <div className="text-zinc-800 text-md font-bold">Growth charts</div>
+                            </div>
+                            <div className="flex-col justify-start items-start gap-3 flex">
+                                <div className="text-zinc-800 text-base font-bold ">#8 visits</div>
+                                <div className="text-zinc-800 text-opacity-70 text-md font-medium leading-none">Growth chart data is incomplete for the last<br />8 visits, Enter data via visits.</div>
+                            </div>
+                            <div className="p-2.5 justify-center items-center gap-2.5 ml-auto">
+                                <div className="text-zinc-800 text-md font-bold">Ok, thanks</div>
+                            </div>
+                        </div>
+                        <div className="border-2 w-full  flex-col justify-start items-start gap-2.5 inline-flex p-2 m-2 bg-gray-100 rounded-xl">
                             <div className="w-[250px] justify-start items-center gap-1.5 inline-flex">
                                 <div className="w-[35px] h-[35px] bg-stone-100 rounded-full flex items-center justify-center">
                                     <ArrowBigUp />
@@ -281,7 +280,7 @@ function index() {
                                 <div className="text-zinc-800 text-md font-bold">Ok, thanks</div>
                             </div>
                         </div>
-                        <div className="border-2 w-fit flex-col justify-start items-start gap-2.5 inline-flex p-2 m-2 bg-gray-100 rounded-xl">
+                        <div className="border-2 w-full  flex-col justify-start items-start gap-2.5 inline-flex p-2 m-2 bg-gray-100 rounded-xl">
                             <div className="w-[250px] justify-start items-center gap-1.5 inline-flex">
                                 <div className="w-[35px] h-[35px] bg-stone-100 rounded-full flex items-center justify-center">
                                     <ArrowBigUp />
@@ -296,7 +295,7 @@ function index() {
                                 <div className="text-zinc-800 text-md font-bold">Ok, thanks</div>
                             </div>
                         </div>
-                        <div className="border-2 w-fit flex-col justify-start items-start gap-2.5 inline-flex p-2 m-2 bg-gray-100 rounded-xl">
+                        <div className="border-2 w-full  flex-col justify-start items-start gap-2.5 inline-flex p-2 m-2 bg-gray-100 rounded-xl">
                             <div className="w-[250px] justify-start items-center gap-1.5 inline-flex">
                                 <div className="w-[35px] h-[35px] bg-stone-100 rounded-full flex items-center justify-center">
                                     <ArrowBigUp />
@@ -311,22 +310,7 @@ function index() {
                                 <div className="text-zinc-800 text-md font-bold">Ok, thanks</div>
                             </div>
                         </div>
-                        <div className="border-2 w-fit flex-col justify-start items-start gap-2.5 inline-flex p-2 m-2 bg-gray-100 rounded-xl">
-                            <div className="w-[250px] justify-start items-center gap-1.5 inline-flex">
-                                <div className="w-[35px] h-[35px] bg-stone-100 rounded-full flex items-center justify-center">
-                                    <ArrowBigUp />
-                                </div>
-                                <div className="text-zinc-800 text-md font-bold">Growth charts</div>
-                            </div>
-                            <div className="flex-col justify-start items-start gap-3 flex">
-                                <div className="text-zinc-800 text-base font-bold ">#8 visits</div>
-                                <div className="text-zinc-800 text-opacity-70 text-md font-medium leading-none">Growth chart data is incomplete for the last<br />8 visits, Enter data via visits.</div>
-                            </div>
-                            <div className="p-2.5 justify-center items-center gap-2.5 ml-auto">
-                                <div className="text-zinc-800 text-md font-bold">Ok, thanks</div>
-                            </div>
-                        </div>
-                        <div className="border-2 w-fit flex-col justify-start items-start gap-2.5 inline-flex p-2 m-2 bg-gray-100 rounded-xl">
+                        <div className="border-2 w-full  flex-col justify-start items-start gap-2.5 inline-flex p-2 m-2 bg-gray-100 rounded-xl">
                             <div className="w-[250px] justify-start items-center gap-1.5 inline-flex">
                                 <div className="w-[35px] h-[35px] bg-stone-100 rounded-full flex items-center justify-center">
                                     <ArrowBigUp />
@@ -343,7 +327,6 @@ function index() {
                         </div>
                     </div>
                 </div>
-
             </div >
         </>
     );
