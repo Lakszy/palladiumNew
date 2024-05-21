7c7822850dc7d22e90fc1052ad9cc5e8dee53b7f
Lakshay Khattar <lakshay.khattar@visual-alpha.com>
2024-05-21 21:45:07 +0530

    DB Full Screen Loader

--- app/page.tsx
+++ app/page.tsx
@@ -7,38 +7,51 @@ import { TabsDemoFalse } from "@/components/TabsDemoFalse";
 import { useAccount } from "wagmi";
 import ConnectWalletDefault from "@/components/ConnectWalletDefault";
 import NotMinted from "@/components/NotMinted";
+import FullScreenLoader from "@/components/FullScreenLoader";
 
 export default function Home() {
   const { address } = useAccount();
   const [userExists, setUserExists] = useState(false);
+  const [afterLoad, setAfterload] = useState(false);
 
   useEffect(() => {
     if (address) {
-      fetch(`https://api.palladiumlabs.org/users/testnetWhitelist/${address}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setUserExists(data.userExists);
-        })
+      setAfterload(true)
+      fetch(`https://api.palladiumlabs.org/users/testnetWhitelist/${address}`).then((response) => response.json()).then((data) => {
+        setUserExists(data.userExists);
+        setAfterload(false)
+      })
         .catch((error) => {
           console.error("Error fetching data:", error);
         });
     }
   }, [address]);
-  
+
   return (
-    <div className="grid h-screen font-mono font-extrabold mainT w-full sm:grid-cols-[max-content_1fr] overflow text-white">
-      {userExists ? <TabsDemo /> : <TabsDemoFalse />}
-      <div className="body text-black  overflow-y-scroll ">
-        <div className="sticky z-50 mainT top-0  overflow-auto">
-          <NavBar />
-        </div>
-        <div className=" w-full " style={{ backgroundColor: "#272315" }}>
-          {address ? (
-            userExists ? (<CardDemo userExists={userExists} />
-            ) : (<NotMinted />)
-          ) : (<ConnectWalletDefault />)}
-        </div>
-      </div>
-    </div>
+    <>
+      {
+        afterLoad ? (
+          <FullScreenLoader />
+        ) : (
+          <div className="grid h-screen font-mono font-extrabold mainT w-full sm:grid-cols-[max-content_1fr] overflow text-white">
+            {userExists ? <TabsDemo /> : <TabsDemoFalse />}
+            <div className="body text-black  overflow-y-scroll ">
+              <div className="sticky z-50 mainT top-0  overflow-auto">
+                <NavBar />
+              </div>
+              <div className=" w-full " style={{ backgroundColor: "#272315" }}>
+                {address ? (
+                  userExists ? (
+                    <CardDemo userExists={userExists} />
+                  ) : (
+                    <NotMinted />
+                  )
+                ) : (<ConnectWalletDefault />)}
+              </div>
+            </div>
+          </div>
+        )
+      }
+    </>
   );
 }
--- components/NotMinted.tsx
+++ components/NotMinted.tsx
@@ -1,20 +1,14 @@
-import React from "react";
 
+import React from "react";
 import Image from "next/image";
 import nouser from "../app/assets/images/nouser.png";
 import discord from "../app/assets/images/Group 857.png";
 
 const NotMinted = () => {
   return (
-    <div
-      className="h-screen w-full pb-16 sm:pb-1 max-sm:px-5"
-      style={{ backgroundColor: "#272315" }}
-    >
+    <div className="h-screen w-full pb-16 sm:pb-1 max-sm:px-5" style={{ backgroundColor: "#272315" }}>
       <div className=" md:w-full h-full flex flex-col items-center justify-center md:mt-[-5rem]">
-        <div
-          className="max-sm:w-[20rem]  md:px-[2rem] pb-[2rem] flex flex-col items-center justify-center"
-          style={{ backgroundColor: "#2D2A1C" }}
-        >
+        <div className="max-sm:w-[20rem]  md:px-[2rem] pb-[2rem] flex flex-col items-center justify-center" style={{ backgroundColor: "#2D2A1C" }}>
           <div className="lg:mt-[2rem]">
             {" "}
             <Image src={nouser} alt="home" className=" lg:w-full h-full" />
@@ -26,12 +20,7 @@ const NotMinted = () => {
             Stay tuned on our Discord for next phase of whitelisting
           </p>
           <span>
-            <a
-              className="w-full md:w-[10rem] title-text bg-lightyellow text-black text-lg   mt-4 md:mt-[2rem] px-4 py-2 text-center flex flex-row border border-lightyellow items-center justify-center place-items-center gap-x-2"
-              href="https://discord.com/invite/9MMEyJ4JDz"
-              target="_blank"
-              // onClick={handleDiscordClick}
-            >
+            <a className="w-full md:w-[10rem] title-text bg-lightyellow text-black text-lg   mt-4 md:mt-[2rem] px-4 py-2 text-center flex flex-row border border-lightyellow items-center justify-center place-items-center gap-x-2" href="https://discord.com/invite/9MMEyJ4JDz" target="_blank"            >
               {" "}
               <Image src={discord} alt="discord" width={30} height={30} />
               <b className="title-text font-thin">DISCORD </b>
